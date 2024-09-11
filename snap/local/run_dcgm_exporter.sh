349ee7a7c5df640431be848a7d9d48390c7b1239
Deezzir <deezzir@gmail.com>
2024-09-11 11:50:52 -0400

    Make dcgm-exporter listen address configurable

---
+++ snap/local/run_dcgm_exporter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+# Build the argument list for the dcgm-exporter command
+args=()
+
+# Add the dcgm-exporter-port option if it is set. Default: “:9400”
+dcgm_exporter_listen="$(snapctl get dcgm_exporter_listen)"
+
+if [ -n "$dcgm_exporter_listen" ]; then
+    args+=("-a" "$dcgm_exporter_listen")
+fi
+
+exec "$SNAP/bin/dcgm-exporter" "${args[@]}"
