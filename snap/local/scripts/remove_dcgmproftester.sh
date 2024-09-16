073b770dadad51cb08d0d4188dd13808206ce6c4
Gabriel Cocenza <gabriel.cocenza@canonical.com>
2024-09-16 15:44:05 -0300

    Apply suggestions

--- snap/local/scripts/remove_dcgmproftester.sh
+++ snap/local/scripts/remove_dcgmproftester.sh
@@ -7,18 +7,18 @@ FILES_TO_REMOVE=(
 )
 
 
-echo "Removing dcgmproftester libs"
+echo "Removing dcgmproftester files"
 
 
-for lib in "${FILES_TO_REMOVE[@]}"; do
-    find "$SNAPCRAFT_PRIME" -type f -name "$lib" | while read -r file; do
+for pattern in "${FILES_TO_REMOVE[@]}"; do
+    find "$SNAPCRAFT_PRIME" -type f -name "$pattern" | while read -r file; do
         echo "Removing file $file"
         rm -f "$file"
     done
 done
 
 
-echo "Removing libs that are missing cuda libs"
+echo "Removing cuda directories"
 find "$SNAPCRAFT_PRIME" -type d -name "cuda[0-9]*" | while read -r dir; do
     echo "Removing directory $dir"
     rm -rf "$dir"
