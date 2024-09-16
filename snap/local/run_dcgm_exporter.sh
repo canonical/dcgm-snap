2892b99489ca8b53b912862d65e3510cad2c35da
Deezzir <deezzir@gmail.com>
2024-09-16 19:49:24 -0400

    Add custom metrics configuration option

--- snap/local/run_dcgm_exporter.sh
+++ snap/local/run_dcgm_exporter.sh
@@ -6,9 +6,17 @@ args=()
 
 # Add the dcgm-exporter-address option if it is set. Default: “:9400”
 dcgm_exporter_address="$(snapctl get dcgm-exporter-address)"
+# Add the dcgm-exporter-metrics-file option if it is set.
+dcgm_exporter_metrics_file="$(snapctl get dcgm-exporter-metrics-file)"
+dcgm_exporter_metrics_file_path="$SNAP_COMMON/etc/dcgm-exporter/$dcgm_exporter_metrics_file"
 
 if [ -n "$dcgm_exporter_address" ]; then
     args+=("-a" "$dcgm_exporter_address")
+fi
+
+# File should be available in the snap data directory under $SNAP_COMMON/etc/dcgm-exporter
+if [[ -n "$dcgm_exporter_metrics_file" && -f "$dcgm_exporter_metrics_file_path" ]]; then
+    args+=("-f" "$dcgm_exporter_metrics_file_path")
 fi
 
 exec "$SNAP/bin/dcgm-exporter" "${args[@]}"
