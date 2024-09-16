e8fa48cc0d29da43183122df4d32d131f4738c90
Gabriel Cocenza <gabriel.cocenza@canonical.com>
2024-09-16 13:13:18 -0300

    - not remove unused libs.

--- snap/local/scripts/remove_dcgmproftester.sh
+++ snap/local/scripts/remove_dcgmproftester.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
 
-LIBS_TO_REMOVE=(
+FILES_TO_REMOVE=(
     "dcgmproftester*"
     "libdcgm_cublas_proxy*"
-    # libs that library linter warn that is unused
-    # see https://snapcraft.io/docs/linters-library
-    "libnvperf_dcgm_host.so"
-    "libnvml_injection.so.1.0"
-    "libdcgmmodulesysmon.so.*"
-    "libdcgmmoduleprofiling.so.*"
-    "libdcgmmodulepolicy.so.*"
-    "libdcgmmodulenvswitch.so.*"
-    "libdcgmmoduleintrospect.so.*"
-    "libdcgmmodulehealth.so.*"
-    "libdcgmmodulediag.so.*"
-    "libdcgmmoduleconfig.so.*"
 )
 
 
 echo "Removing dcgmproftester libs"
 
 
-for lib in "${LIBS_TO_REMOVE[@]}"; do
+for lib in "${FILES_TO_REMOVE[@]}"; do
     find "$SNAPCRAFT_PRIME" -type f -name "$lib" | while read -r file; do
         echo "Removing file $file"
         rm -f "$file"
