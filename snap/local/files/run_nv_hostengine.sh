0fab66559682d35138438c5c428adee777eb391a
Deezzir <deezzir@gmail.com>
2024-09-17 21:16:37 -0400

    Fix

--- snap/local/files/run_nv_hostengine.sh
+++ snap/local/files/run_nv_hostengine.sh
@@ -12,7 +12,7 @@ if [ -n "$nv_hostengine_port" ]; then
 fi
 
 # Check if dcgm-exporter service is running and restart it
-echo "dcgm-exporter service status: $dcgm_exporter_status"
+dcgm_exporter_status=$(snapctl services dcgm.dcgm-exporter | awk '/dcgm\.dcgm-exporter/ {print $3}')
 if [ "$dcgm_exporter_status" == "active" ]; then
     snapctl restart dcgm.dcgm-exporter
 fi
