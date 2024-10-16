1ac9fa2cbc5c260081e0d0a1334f677dcf6a7a97
Deezzir <deezzir@gmail.com>
2024-10-16 14:28:28 -0400

    Simplify dcgmproftester wrapper

--- snap/local/files/run_dcgmproftester.sh
+++ snap/local/files/run_dcgmproftester.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-# Check if the argument is one of 10, 11, or 12
 if [[ "$1" != "10" && "$1" != "11" && "$1" != "12" ]]; then
     exit 1
 fi
 
+BINARY="$SNAP/usr/bin/dcgmproftester$1"
 ARCH=$(uname -m)
 
-if [[ "$ARCH" == "x86_64" ]]; then
-    exec "$SNAP/usr/bin/dcgmproftester$1"
-elif [[ "$ARCH" == "aarch64" ]]; then
-    if [[ "$1" == "10" ]]; then
-        exec "$SNAP/usr/bin/dcgmproftester10"
-    else
-        echo "ERROR: dcgmproftester$1 is not supported on ARM64 platform."
-    fi
+if [[ -x "$BINARY" ]]; then
+    exec "$BINARY"
+else
+    echo "ERROR: dcgmproftester$1 is not supported on the $ARCH platform."
+    exit 1
 fi
-
-exit 1
