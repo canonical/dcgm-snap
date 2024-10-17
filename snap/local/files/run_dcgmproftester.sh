527a87da9c58117685f44c5edeb5df46c71d941c
Deezzir <deezzir@gmail.com>
2024-10-17 11:07:31 -0400

    Update return code

--- snap/local/files/run_dcgmproftester.sh
+++ snap/local/files/run_dcgmproftester.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # The wrapper is needed so the build does not fail on ARM, as there is no dcgmproftester10 in the ARM DCGM deb package
 
 if [[ "$1" != "10" && "$1" != "11" && "$1" != "12" ]]; then
-    exit 1
+    exit 127
 fi
 
 BINARY="$SNAP/usr/bin/dcgmproftester$1"
